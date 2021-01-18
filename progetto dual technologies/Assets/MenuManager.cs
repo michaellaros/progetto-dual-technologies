1fed33b1ed49378cb28e22545c8b1fbaff11ca7d
michaellaros <56259544+michaellaros@users.noreply.github.com>
2021-01-18 16:19:18 +0100

    start

---
+++ progetto dual technologies/Assets/MenuManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+   public void Playgame()
+    {
+        SceneManager.LoadScene("gamesearch");
+    }
+
+    public void Quitgame()
+    {
+        Application.Quit();
+    }
+
+    public void Shop()
+    {
+        SceneManager.LoadScene("shop");
+    }
+}
