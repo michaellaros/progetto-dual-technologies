04fd5a9e0a5d5c520aef4d76e1442462846e414e
michaellaros <56259544+michaellaros@users.noreply.github.com>
2021-01-22 18:56:28 +0100

    levette

---
+++ progetto dual technologies/Assets/Left.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Left : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform pressedRotationpoint;
+    Vector3 originalRotation;
+    public float movementSpeed;
+
+    void Start()
+    {
+        originalRotation = transform.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            iTween.RotateTo(gameObject, iTween.Hash("rotation", pressedRotationpoint.eulerAngles, "easeType", "easeOutQuint", "time", movementSpeed)); 
+        }
+        if (Input.GetKeyUp(KeyCode.K))
+        {
+            iTween.RotateTo(gameObject, iTween.Hash("rotation", originalRotation, "easeType", "easeOutQuint", "time", movementSpeed));
+        }
+    }
+}
---
+++ progetto dual technologies/Assets/Right.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Right : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform pressedRotationpoint;
+    Vector3 originalRotation;
+    public float movementSpeed;
+
+    void Start()
+    {
+        originalRotation = transform.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            iTween.RotateTo(gameObject, iTween.Hash("rotation", pressedRotationpoint.eulerAngles, "easeType", "easeOutQuint", "time", movementSpeed)); 
+        }
+        if (Input.GetKeyUp(KeyCode.L))
+        {
+            iTween.RotateTo(gameObject, iTween.Hash("rotation", originalRotation, "easeType", "easeOutQuint", "time", movementSpeed));
+        }
+    }
+}
